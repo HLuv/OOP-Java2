b2f79c4d16769a5646a2907fc147b5452bd3f084
HLuv <korolishut220@gmail.com>
2026-06-15 02:47:07 +0300

    fix error 403 after logging in as user

--- src/main/java/com/example/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/example/helpdesk/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/admin/tickets", true)
+                        .defaultSuccessUrl("/", true)
                         .permitAll()
                 )
                 .logout(logout -> logout
